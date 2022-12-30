853c2bd0115c64412817ebf6393204acff6d2827
Eero Vaarnas <eero.vaarnas@visma.com>
2022-12-30 10:30:54 +0200

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited <code>src/App.js</code> and save to reloaded.
+          Edited <code>src/App.js</code> and saved to reload.
         </p>
         <a
           className="App-link"
